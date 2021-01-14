975b6414c6853e0e5b29c51b3f9e48a85a06865f
Lena Anna Ebner <lebner.mmt-b2019@fh-salzburg.ac.at>
2021-01-14 23:39:12 +0100

    Solve conflicts

--- src/php/index.php
+++ src/php/index.php
@@ -2,11 +2,8 @@
  $context          = Timber::context();
  $context['posts'] = new Timber\PostQuery();
  $context['post'] = new Timber\Post();
-// $context['foo']   = 'bar';
-
 
  $templates        = array( 'index.twig' );
-// if ( is_home() ) {
-// 	array_unshift( $templates, 'front-page.twig', 'home.twig' );
-// }
 Timber::render( $templates, $context );
+
+?>
