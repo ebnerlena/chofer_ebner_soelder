8a159c37e5f1f5f2ee52cc9be5f6061a195ca8d7
Lena Anna Ebner <lebner.mmt-b2019@fh-salzburg.ac.at>
2021-01-14 21:53:14 +0100

    Fix merging

--- public/wp-content/themes/chofer/functions.php
+++ public/wp-content/themes/chofer/functions.php
@@ -8,8 +8,8 @@ function enqueue_scripts_and_styles()
     // wp_enqueue_style('main_style', get_template_directory_uri() . '/sass/main.css', array(), null, false);
     // wp_enqueue_script('main-js', get_template_directory_uri() . '/js/app.js', array(), null, false);
 
-    wp_enqueue_style('main-styles', get_template_directory_uri() . '/main.897f6fbf0eaa9b227f63.css?3066783ebee83b233b84', array(), null, false);
-    wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js?3066783ebee83b233b84', array(), null, false);
+    wp_enqueue_style('main-styles', get_template_directory_uri() . '/main.c3fdbec86a253ee42348.css?3f9f5d7a5a909c3aaf5d', array(), null, false);
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js?3f9f5d7a5a909c3aaf5d', array(), null, false);
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts_and_styles');
 
