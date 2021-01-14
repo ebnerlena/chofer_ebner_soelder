8f112c47339ddbab52adf6061dcf4ef5dcea0b2f
Lena Anna Ebner <lebner.mmt-b2019@fh-salzburg.ac.at>
2021-01-14 22:14:35 +0100

    Fix border radius in chrome @ audio tag

--- public/wp-content/themes/chofer/functions.php
+++ public/wp-content/themes/chofer/functions.php
@@ -8,8 +8,8 @@ function enqueue_scripts_and_styles()
     // wp_enqueue_style('main_style', get_template_directory_uri() . '/sass/main.css', array(), null, false);
     // wp_enqueue_script('main-js', get_template_directory_uri() . '/js/app.js', array(), null, false);
 
-    wp_enqueue_style('main-styles', get_template_directory_uri() . '/main.c3fdbec86a253ee42348.css?3f9f5d7a5a909c3aaf5d', array(), null, false);
-    wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js?3f9f5d7a5a909c3aaf5d', array(), null, false);
+    wp_enqueue_style('main-styles', get_template_directory_uri() . '/main.274b5a58347e686811e2.css?992ea58f33094104179e', array(), null, false);
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js?992ea58f33094104179e', array(), null, false);
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts_and_styles');
 
