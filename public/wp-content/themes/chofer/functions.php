5d0190dd75db665222b429385644ea7614871fae
simon <soelder@hotmail.com>
2021-01-20 12:19:58 +0100

    Update footer style

--- public/wp-content/themes/chofer/functions.php
+++ public/wp-content/themes/chofer/functions.php
@@ -8,8 +8,8 @@ function enqueue_scripts_and_styles()
     // wp_enqueue_style('main_style', get_template_directory_uri() . '/sass/main.css', array(), null, false);
     // wp_enqueue_script('main-js', get_template_directory_uri() . '/js/app.js', array(), null, false);
 
-    wp_enqueue_style('main-styles', get_template_directory_uri() . '/main.55375d03666fb289499f.css?8bc6295fe9c41df6d6ba', array(), null, false);
-    wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js?8bc6295fe9c41df6d6ba', array(), null, false);
+    wp_enqueue_style('main-styles', get_template_directory_uri() . '/main.3897f45e04b0c6fc8e33.css?0cb07f198015b8753c39', array(), null, false);
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js?0cb07f198015b8753c39', array(), null, false);
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts_and_styles');
 
